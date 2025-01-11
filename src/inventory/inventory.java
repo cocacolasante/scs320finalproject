22de0fe8ad86d5fd6ba4eb8bbe77730bd6534813
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2025-01-11 17:17:54 -0500

    comments

--- src/inventory/inventory.java
+++ src/inventory/inventory.java
@@ -9,44 +9,56 @@ import src.automobile.automobile;
 public class inventory {
     public static void main(String[] args) {
         try{
+            // init scanner to get user inoput
             Scanner scanner = new Scanner(System.in);
 
+            // initialize an instance of the car class
             automobile car = new automobile("honda", "Accord", "black", 2025);
 
+            // get car info in string, split string, loop through string array and print info to terminal
             String carinfo = car.listing();
             String[] splitInfo = carinfo.split(",");
             for(String info : splitInfo){
                 System.out.println(info);
             }
 
+            // remove the car
             boolean removed = car.removeCar();
+            // check that the remove car method returned true, if returned false, error occured
             if(!removed){
                 System.out.println("Car was not removed");
             }
             System.out.println("Car removed");
 
+            // add new ar
             String addedCar = car.addCar("infiniti", "G37", "white", 2013);
             System.out.println(addedCar);
 
+            
             String addedCarInfo = car.listing();
-            String[] addedCarSplit = carinfo.split(",");
+            String[] addedCarSplit = addedCarInfo.split(",");
             for(String info : addedCarSplit){
                 System.out.println(info);
             }
 
-            String updateCar = car.updateVehicle("Infiniti", "Q50", "White", 2016);
+            // update car info
+            car.updateVehicle("Infiniti", "Q50", "White", 2016);
             String updateCarInfo = car.listing();
-            String[] updateCarSplit = carinfo.split(",");
-            for(String info : addedCarSplit){
+            String[] updateCarSplit = updateCarInfo.split(",");
+            for(String info : updateCarSplit){
                 System.out.println(info);
             }
+
+            // drive car 1k miles to increase milage
+            car.driveCar(1000);
 
             boolean isRunning = true;
-
+            // create a while loop to capture user input until valid response or quit command
             while(isRunning){
                 System.out.println("Would you like to save information to a file? (Y/N)");
                 String userInput = scanner.nextLine().trim().toLowerCase();
                 if(userInput.equalsIgnoreCase("y")){
+                    // prints data to text file
                     try {
                         String filePath = "./carInfo.txt";
                         File file = new File(filePath);
@@ -60,8 +72,9 @@ public class inventory {
                         }
                         writer.close();
             
-                        System.out.println("Vehicle information printed to: " + filePath); 
-                        break;
+                        System.out.println("Vehicle information printed to: " + filePath);
+                        isRunning = false;
+                        
                     } catch (IOException e) {
                         System.out.println("Failed to print information to file: " + e.getMessage()); 
                     }
@@ -69,10 +82,12 @@ public class inventory {
                     
                 }else if(userInput.equalsIgnoreCase("n")){
                     System.out.println("Not saving info, exiting program now");
-                    break;
+                    isRunning = false;
+                    
                 }else if(userInput.equalsIgnoreCase("q") | userInput.equalsIgnoreCase("quit")){
                     System.out.println("Exiting program now");
-                    break;
+                    isRunning = false;
+                    
                 } else{
                     System.out.println("invalid entry");
                 }
@@ -82,5 +97,7 @@ public class inventory {
         }catch(Exception e){
             System.err.println(e);
         }
+
+        System.out.println("Thanks for using, program exiting");
     }
 }
